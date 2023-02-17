4f7ed77b69a9b9e8f92c66a2c4dcb5e73e67c213
Gmasola <76915582+Gmasola@users.noreply.github.com>
2023-02-17 09:44:31 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-require('dotenv').config({ path: '/etc/secret/.env' })
+require('dotenv').config({ path: '/etc/secrets/.env' })
 
 /**
  * -------------- DATABASE ----------------
